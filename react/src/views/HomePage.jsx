a08d3b096fb986e4923559fe5c028c2c0c67a094
Kris <kris@webvalue.nl>
2024-01-25 17:02:53 +0100

    login

--- react/src/views/HomePage.jsx
+++ react/src/views/HomePage.jsx
@@ -1,12 +1,22 @@
 import "./HomePage.scss";
 import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios-client";
 
 const HomePage = () =>{
-    const {user} = useStateContext();
+    const {user, setUser, setToken} = useStateContext();
+
+    const onLogout = () =>{
+        axiosClient.post('/logout')
+            .then(() =>{
+                setUser({})
+                setToken(null)
+            })
+    }
 
     return (
         <div className="home">
             <h1>Home</h1>
+            <button onClick={onLogout}>logout</button>
         </div>
     );
 }
--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,12 @@
 import "./Login.scss";
-import {useState, useEffect, useRef} from "react";
+import {useState, useRef} from "react";
 import axiosClient from "../axios-client.js";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
 import { Link, Navigate } from "react-router-dom";
+import background from "../assets/beechmore-books-0S2rRstB_9M-unsplash.jpg"
+import {ReactSVG} from "react-svg";
+import Vinkje from "../assets/vinkje.svg";
+
 export default function Login() {
     const { token, setUser, setToken} = useStateContext()
     if (token) {
@@ -40,34 +44,48 @@ export default function Login() {
     }
 
     return (
-        <div className="centerlogin">
-        <section className="loginform">
-        <h2 className="loginform__h2">Log In</h2>
-        {errors && <div className="loginform__errors">
-            {Object.keys(errors).map(key => (
-                <p className="loginform__error" key={key}>{errors[key][0]}</p>
-            ))
-            }
-        </div>}
-        <form className="loginform__form" onSubmit={onSubmit} >
-            <div className="loginform__form__inputwrapper">
-                <label className="loginform__form__inputwrapper__label" htmlFor="username">Username</label>
-                <input ref={emailRef} className="loginform__form__inputwrapper__input input" type="text" name="username" id="username" placeholder="Uw gebruikersnaam"/>
-            </div>
-            <div className="loginform__form__inputwrapper">
-                <label className="loginform__form__inputwrapper__label" htmlFor="password">Password</label>
-                <input ref={pwdRef} className="loginform__form__inputwrapper__input input" type="password" name="password" id="password" placeholder="Uw wachtwoord"/>
-            </div>
-            {/* <div className="loginform__form__loginOptionsWrapper">
-                <div className="loginOptionsWrapper__inputWrapper">
-                    <input className="loginOptionsWrapper__inputWrapper__input" type="checkbox" name="staysignedin" id="staysignedin"/>
-                    <label className="loginOptionsWrapper__inputWrapper__label" htmlFor="staysignedin">Ingelogd blijven</label>
+        <div className="login">
+            <div className="leftLoginPane" style={{backgroundImage: `url(${background})`}}>
+                <div className="textWrapper">
+                        <div className="textWrapper__div">
+                        <h2 className="textWrapper__div__h2">RecipeKeeper</h2>
+                        <h1 className="textWrapper__div__h1">Altijd en overal jouw projecten inzichtelijk.</h1>
+                    </div>
                 </div>
-            </div> */}
-            <Link to={"/signup"} className="loginform__form__noaccountyet">Don't have an account yet?</Link>
-            <button className="loginform__form__loginbutton" onClick={onSubmit}>Inloggen</button>
-        </form>
-        </section>
+            </div>
+            <div className="login__rightpane">
+                <div className="loginform">
+                    <h2 className="loginform__h2">Inloggen</h2>
+                    {errors && <div className="loginform__errors">
+                        {Object.keys(errors).map(key => (
+                            <p className="loginform__error" key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>}
+                    <div className="loginform__form">
+                        <div className="loginform__form__inputWrapper">
+                            <label className="loginform__form__inputWrapper__label" htmlFor="username">Gebruikersnaam</label>
+                            <input ref={emailRef} className="loginform__form__inputWrapper__input" type="text" name="username" id="username" placeholder="Uw gebruikersnaam"/>
+                        </div>
+                        <div className="loginform__form__inputWrapper">
+                            <label className="loginform__form__inputWrapper__label" htmlFor="password">Wachtwoord</label>
+                            <input ref={pwdRef} className="loginform__form__inputWrapper__input" type="password" name="password" id="password" placeholder="Uw wachtwoord"/>
+                        </div>
+                        <div className="loginform__form__loginOptionsWrapper">
+                                <input className="loginOptionsWrapper__inputWrapper__input loginOptionsWrapper__inputWrapper__input--staysignedin" type="checkbox" name="staysignedin" id="staysignedin"/>
+                                <label className="loginOptionsWrapper__inputWrapper__label loginOptionsWrapper__inputWrapper__label--staysignedin" htmlFor="staysignedin">
+                                    Ingelogd blijven
+                                
+                                    <span className="loginOptionsWrapper__inputWrapper__span">
+                                        <ReactSVG src={Vinkje} alt="vinkje"  className="loginOptionsWrapper__inputWrapper__span--icon"/>
+                                    </span>
+                                    
+                                </label>
+                                <Link to={"/signup"} className="loginOptionsWrapper__forgotpwd">Wachtwoord vergeten?</Link>
+                        </div>
+                        <button className="loginform__form__loginButton btn" onClick={onSubmit}>Inloggen</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
